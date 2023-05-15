eb045fb8745eeab5de0acef0abf30d803a466f91
Esteban <acevedoesteban999@gmail.com>
2023-05-15 10:17:59 +0300

    -

--- update.sh
+++ update.sh
@@ -1,25 +1,31 @@
 #! /bin/bash
+#echo ""
+#echo "========== Iniciando Proceso de Actualización =========="
+#echo ""
+#cd ../opt/python/django/Stickers
+#./update.sh
+
 echo ""
-echo "===== Accediendo a la Carpeta de Stickers ====="
+echo "========== Accediendo a la Carpeta de Stickers =========="
 echo ""
 cd ../opt/python/django/Stickers
 echo ""
-echo "===== Accediendo al Entorno Virtual ====="
+echo "========== Accediendo al Entorno Virtual =========="
 echo ""
 source .venv/bin/activate
 echo ""
-echo "===== Actualizando de Github.com/Esteban1914/Stickers/ ====="
+echo "========== Actualizando remotamente de Github.com/Esteban1914/Stickers/ =========="
 echo ""
 git pull origin master
 echo ""
-echo "===== Haciendo Migraciones en la Base de Datos ====="
+echo "========== Haciendo Migraciones en la Base de Datos =========="
 echo ""
 python manage.py makemigrations
 python manage.py migrate
 echo ""
-echo "===== Reiniciando Apache ====="
+echo "========== Reiniciando Apache =========="
 echo ""
 systemctl restart apache2
 echo ""
-echo "===== Completado! Se ha actualizado correctamente ====="
+echo "========== Completado! Se ha actualizado correctamente =========="
 echo ""
